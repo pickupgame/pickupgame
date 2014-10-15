3185dbef069ad935ac6f5d79a8bea8390491e495
Jacob Sovoda <jsovoda@umich.edu>
2014-10-15 10:36:20 -0400

    user interface and layout

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+	<title>Pickup Game &copy;</title>
+	<link rel="stylesheet" type="text/css" href="main.css" title="main">
+</head>
+<body>
+	[body]
+	<div id="container">
+		[container]
+		<div id="map">
+			[map goes here]
+		</div>
+		<div id="header">
+			[header]
+			<div id="logo">
+				[logo]
+			</div>
+
+			<div id="navigation">
+				navigation
+				<ul>
+					<li>Home</li>
+					<li>Login/Logout</li>
+					<li>Browse</li>
+				</ul>
+			</div>
+
+		</div>
+
+		<div id="content">
+			[content]
+			<div id="dynamic_content">
+				<p>[dynamic]</p>
+				<div id="dynamic_left">
+					[dynamic left]
+				</div>
+				<div id="dynamic_right">
+					[dynamic right]
+				</div>
+			</div>
+		</div>
+
+		<div id="right">
+			[right]
+			<div id="calendar">
+				[calendar goes here]
+			</div>
+		</div>
+
+		<div id="footer">
+			[footer]
+		</div>
+	</div>
+
+</body>
+</html>
