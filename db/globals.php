2765ca90459a310c7074a4bbe278c242cb331802
Jacob Sovoda <jsovoda@umich.edu>
2014-11-19 10:33:22 -0500

    changed globals to add aws environment

--- db/globals.php
+++ db/globals.php
@@ -1,4 +1,12 @@
 <?php
+
+//aws
+$GLOBALS['host'] = "pickupgameapp.cmkzt5hznaki.us-east-1.rds.amazonaws.com:3306";
+$GLOBALS['db_username'] = 'cis435pickupgame';
+$GLOBALS['db_password'] = '12345678';
+$GLOBALS['db_name'] = 'pickupgamedatabase'; 
+
+//local
 $GLOBALS['host'] = "127.0.0.1";
 $GLOBALS['db_username'] = 'root';
 $GLOBALS['db_password'] = '';
