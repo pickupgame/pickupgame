2fceceba2a9253ef7c112ac463f936afef95b832
Jacob Sovoda <jsovoda@umich.edu>
2014-11-19 11:38:24 -0500

    added aws globals

--- db/globals.php
+++ db/globals.php
@@ -6,9 +6,9 @@ $GLOBALS['db_username'] = 'cis435pickupgame';
 $GLOBALS['db_password'] = '12345678';
 $GLOBALS['db_name'] = 'pickupgamedatabase'; 
 
-//local
-$GLOBALS['host'] = "127.0.0.1";
-$GLOBALS['db_username'] = 'root';
-$GLOBALS['db_password'] = '';
-$GLOBALS['db_name'] = 'pickupgame'; 
-?>
+// //local
+// $GLOBALS['host'] = "127.0.0.1";
+// $GLOBALS['db_username'] = 'root';
+// $GLOBALS['db_password'] = '';
+// $GLOBALS['db_name'] = 'pickupgame'; 
+// ?>
