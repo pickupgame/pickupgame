ff53399b6e90c2bf85241b485ef5b63dd2acbe51
unknown <jrussr@umich.edu>
2014-11-22 18:45:28 -0500

    made alot of changes to login pages

---
+++ SecurityQuestionAndAnswer.php
@@ -0,0 +1,60 @@
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<link rel = "stylesheet" type= "text/css" href = "index.css">
+		<title>Forgot Password</title>
+	</head>
+	<body>
+		<?php
+		session_start();
+		include('sql_functions.php');
+		$UserID = isset($_SESSION["UserIDforSecurityQuestion"]) ? $_SESSION[ "UserIDforSecurityQuestion" ] : "";
+		$SecurityQuestion= getSecurityQuestion($UserID);
+		$SecurityAnswer = isset($_POST["SecurityAnswer"]) ? $_POST[ "SecurityAnswer" ] : "";
+		$iserror=false;
+		$formerrors = array("SecurityAnswererror"=> false);
+		$SecurityAnswerIncorrect=false;
+		if ( isset($_POST["submit"]))
+		{
+			if ($SecurityAnswer == "")
+			{
+				$formerrors ["SecurityAnswererror"] = true;
+				$iserror=true;
+			}
+			if (!$iserror)
+			{
+				print(getSecurityAnswer($UserID));
+				if(getSecurityAnswer($UserID)==$SecurityAnswer)
+				{
+					$_SESSION["UserIDforPasswordGet"]=$UserID;
+					header('Location: GetPassword.php');
+					die();
+				}
+				else
+				{
+					$SecurityAnswerIncorrect=true;
+				}
+			}
+		}
+		print("<h1>Forgot Password</h1>");
+		print("<h2>Enter Security Answer</h2>");
+		print( "<form method = 'post' action = 'SecurityQuestionAndAnswer.php' id= 'submission'>");
+		print("<div><label>Question:</label><input type = 'text' name = '' value = '");
+		print(htmlspecialchars($SecurityQuestion, ENT_QUOTES));
+		print("' disabled></div>");
+		print("<div><label>Answer:</label><input type = 'text' name = 'SecurityAnswer' value = {$SecurityAnswer}></div>");
+		if($formerrors["SecurityAnswererror"])
+		{
+			print("<p class = 'error'>Please enter a Security Answer</p>");
+		}
+		if($SecurityAnswerIncorrect)
+		{
+			print("<p class = 'error'>The Security Question and Answer Do Not Match: Please Try Again</p>");
+		}
+		print( "<p class = 'head'><input type = 'submit' name = 'submit' value = 'Validate'>   </form>");
+		print("<form action= 'login.php'>");
+		print("<input type = 'submit' value='Return To Login'></p>");
+		print("</form>");
+		?>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,83 +1,270 @@
-<?php 
-error_reporting(E_ALL);
-session_start();
-$profile['UserID'] = "1235";
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
+
+
 <html>
-<head>
-	<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
-	<title>Pickup Game &copy;</title>
-	<link rel="stylesheet" type="text/css" href="main.css" title="main">
+	<head>
+		<meta charset = "utf-8">
+		<link rel = "stylesheet" type= "text/css" href = "index.css">
+		<title>Registration Form</title>
+	</head>
+	<body>
+		<script type='text/javascript'>
 
-
-<script src="javascript/jquery-1.11.1.js"></script>
-
-<script>
-$(function(){
-	<?php
-		if(isset($_GET['page']) && ($_GET['page'] == 'home' || $_GET['page'] == 'browse'))
-		{
-			echo "$('#dynamic_right').toggle();";
-			echo "$('#dynamic_left').css('width', '940px');";
+		function appear() 
+		{ 
+		    document.getElementById('Change').style.display="inline";
 		}
-	?>
-})
-</script>
+		</script>
+		<form METHOD="LINK" ACTION="studentregistrationlist.php">
+			<INPUT TYPE="submit" VALUE="Click here to see the Current Student List">
+		</form>
+		<h2> Registration Information</h2>
+		<form method = 'post' action = "<?php echo $_SERVER['PHP_SELF']; ?>">
+		<input id = "Change" style = "display:none" type = 'submit' name = 'yes' value = 'Yes, Change My Time'>
+		</form>
+		<?php
+		session_start();
+		// variables used in script
+		$username = "jrussr42";
+		$password = "12345678";
+		$hostname = "jasonrussell435db.cmkzt5hznaki.us-east-1.rds.amazonaws.com:3306"; 
+		$databasename = "umdregistration"; 
+		$portnum=3306;
+		$umid = isset($_POST["umid"]) ? $_POST[ "umid" ] : "";
+		$fname = isset($_POST["fname"]) ? $_POST[ "fname" ] : "";
+		$lname = isset($_POST["lname"]) ? $_POST[ "lname" ] : "";
+		$projname = isset($_POST["projname"]) ? $_POST[ "projname" ] : "";
+		$email = isset($_POST["email"]) ? $_POST[ "email" ] : "";
+		$phone = isset($_POST["phone"]) ? $_POST[ "phone" ] : "";
+		$timeslot = isset($_POST["timeslot"]) ? $_POST[ "timeslot" ] : "";
+		$action = isset($_POST["action"]) ? $_POST[ "action" ] : "";
+		$iserror = false;
+		$duplicaterequest=false;
+		$formerrors = array("umiderror"=> false,"fnameerror"=> false,"lnameerror"=>false,"projnameerror"=>false,
+							"emailerror"=>false,"phoneerror"=>false,"timesloterror"=>false);
+		$inputlist = array( "umid" => "UMID", "fname" => "First Name", "lname" => "Last Name", 
+							"projname" => "Project", "email"=> "Email", "phone" => "Phone");
 
-</head>
-<body>
-	[body]stuff
-	<div id="container">
-		[container]
-		<div id="map">
-			[map goes here]
-		</div>
-		<div id="header">
-			[header]
-			<div id="logo">
-				[logo]
-			</div>
 
-			<div id="navigation">
-				<!-- navigation -->
-				<?php include('nav.php'); ?>
-			</div>
+		$database = mysql_connect($hostname, $username, $password) 
+		 or die("Unable to connect to MySQL");
 
-		</div>
+		//select a database to work with
+		$selected = mysql_select_db($databasename,$database) 
+		  or die("Could not select examples");
+		  print($action);
+		if(isset($_POST["yes"]))
+		{
+			$query = "UPDATE regsection SET SeatsRemaining = SeatsRemaining + 1 WHERE DateAndTime = '".mysql_real_escape_string($_SESSION["previoustimeslot"])."'";
+			if( !( $result=mysql_query($query, $database)))
+			{
+				print ("<p>Could not execute query!</p>");
+				die(mysql_error());
+			}
+			$query = "UPDATE regsection SET SeatsRemaining = SeatsRemaining - 1 WHERE DateAndTime = '".mysql_real_escape_string($_SESSION["timeslot"])."'";
+			if( !( $result=mysql_query($query, $database)))
+			{
+				print ("<p>Could not execute query!</p>");
+				die(mysql_error());
+			}
+			$changingtimeumid=$_SESSION["umid"];
+			$query = "UPDATE student SET TimeSlot = '".mysql_real_escape_string($_SESSION["timeslot"])."' WHERE UMID='$changingtimeumid'";
+			if( !( $result=mysql_query($query, $database)))
+			{
+				print ("<p>Could not execute query!</p>");
+				die(mysql_error());
+			}
+			print("<p><span class = 'success'>You Have successfully changed your Registration Time</span></p>");
+			unset($_POST["yes"]);
+		}
+		if ( isset($_POST["submit"]))
+		{
+			if($umid == "" || (!(preg_match("/^[0-9]{8}$/",$umid))))
+			{
+				$formerrors ["umiderror"] = true;
+				$iserror = true;
+			}
+			if($fname == "" || (!(preg_match("/^[A-z-]+$/",$fname))))
+			{
+				$formerrors ["fnameerror"] = true;
+				$iserror = true;
+			}
+			if($lname == "" || (!(preg_match("/^[A-z-]+$/",$lname))))
+			{
+				$formerrors ["lnameerror"] = true;
+				$iserror = true;
+			}
+			if($projname == "")
+			{
+				$formerrors ["projnameerror"] = true;
+				$iserror = true;
+			}
+			if($email == ""|| (!(preg_match("/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/",$email))))
+			{
+				$formerrors ["emailerror"] = true;
+				$iserror = true;
+			}
+			if( !preg_match("/[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phone))
+			{
+				$formerrors ["phoneerror"] = true;
+				$iserror = true;
+			}
+			if($timeslot == "")
+			{
+				$formerrors ["timesloterror"] = true;
+				$iserror = true;
+			}
+			if(!$iserror)
+			{
+				$_SESSION["umid"]=$umid;
+				$_SESSION["fname"]=$fname;
+				$_SESSION["lname"]=$lname;
+				$_SESSION["phone"]=$phone;
+				$_SESSION["email"]=$email;
+				$_SESSION["projname"]=$projname;
+				$_SESSION["timeslot"]=$timeslot;
+				$result = mysql_query("SELECT UMID, TimeSlot FROM student WHERE UMID = '$umid'");
+				while ($row = mysql_fetch_array($result))
+				{
+					$temp=$row{'UMID'};
+					$previoustimeslot=$row{'TimeSlot'};
+					$duplicaterequest= true;
+					$_SESSION["previoustimeslot"]=$row{'TimeSlot'};
+				}
+				if(!$duplicaterequest)
+				{
+					$query = "INSERT INTO student (UMID, FName, LName, PTitle, Email, Phone, TimeSlot) ".
+							 "VALUES ('$umid', '$fname', '$lname', '$projname', '$email', '" .mysql_real_escape_string( $phone )."', '".mysql_real_escape_string( $timeslot)."')";
+					if( !( $result=mysql_query($query, $database)))
+					{
+						print ("<p>Could not execute query!</p>");
+						die(mysql_error());
+					}
 
-		<div id="content">
-			[content]
-			<div id="dynamic_content">
-				<p>[dynamic]</p>
-				<div id="dynamic_left">
-					[dynamic left]
-					<div id="rateUserUp">
-						<?php 
-						if(isset($profile['UserID']))
-						{
-							include('rateuser.php');
-						}
-					?>
-					</div>
-				</div>
-				<div id="dynamic_right">
-					[dynamic right]
-				</div>
-			</div>
-		</div>
+					$query = "UPDATE regsection SET SeatsRemaining = SeatsRemaining - 1 WHERE DateAndTime = '".mysql_real_escape_string($timeslot)."'";
+					if( !( $result=mysql_query($query, $database)))
+					{
+						print ("<p>Could not execute query!</p>");
+						die(mysql_error());
+					}
+				}
+			}
+		}
+		print( "<form method = 'post' action = 'index.php' id= 'submission'>");
+		if(!$iserror&&!$duplicaterequest&& (isset($_POST["submit"])))
+		{
+			print("<p><span class = 'success'>YOU HAVE SUCCESSFULLY REGISTERED FOR YOUR COURSE</span></p>");
+		}
+		if ( isset($_POST["submit"])&&$duplicaterequest)
+		{
+			foreach($inputlist as $inputname => $inputalt )
+			{
+				print("<div><label>$inputalt:</label><input type = 'text' name = '$inputname' value = '".$$inputname."' disabled>");
+				if ($formerrors[($inputname)."error"] == true )
+				{
+					print("<span class = 'error'>*</span>");
+				}
+				print("</div>");
+			}
+		}
+		else
+		{
+			foreach($inputlist as $inputname => $inputalt )
+			{
+				print("<div><label>$inputalt:</label><input type = 'text' name = '$inputname' value = '".$$inputname."'>");
+				if ($formerrors[($inputname)."error"] == true )
+				{
+					print("<span class = 'error'>*</span>");
+				}
+				print("</div>");
+			}
+		}
+		if ($iserror)
+		{
+			print ("<p class = 'error'>Please fix the following areas marked by an *");
+		}
+		if ($formerrors[ "phoneerror"])
+		{
+			print ("<p class = 'error'>Phone must be in the form 333-333-3333");
+		}
+		if ($formerrors[ "emailerror"])
+		{
+			print ("<p class = 'error'>Email must be in the form john@company.com");
+		}
+		if ($formerrors[ "timesloterror"])
+		{
+			print ("<p class = 'error'>There are no Timeslots left to choose from");
+		}
 
-		<div id="right">
-			[right]
-			<div id="calendar">
-				[calendar goes here]
-			</div>
-		</div>
 
-		<div id="footer">
-			[footer]
-		</div>
-	</div>
+		//connection to the database
+		if( !( $result= mysql_query("SELECT * FROM regsection")))
+					{
+						print ("<p>Could not execute query!</p>");
+						die(mysql_error());
+					}
+		//fetch tha data from the database 
+		$counter=0;
+		while ($row = mysql_fetch_array($result)) 
+		{
+			$timeslotarray[$counter]=$row{'DateAndTime'};
+			$seatsremainingarray[$counter]=$row{'SeatsRemaining'};
+			$counter++;
+		}
+		if ( isset($_POST["submit"])&&$duplicaterequest)
+		{
+			print( "<p>Which Registration Time Would You Like?</p>
+				<select name = 'timeslot' disabled>");
+		}
+		else
+		{
+			print( "<p>Which Registration Time Would You Like?</p>
+				<select name = 'timeslot'>");
+		}
+		
+		foreach(array_combine($timeslotarray, $seatsremainingarray) as $currenttimeslot => $currentseatsremaining)
+		{
+			if($currentseatsremaining>0)
+			{
+				print( "<option" .($currenttimeslot == $timeslot ?" selected>" : ">"). $currenttimeslot . "</option>");
+			}
+		}
+		print( "</select>");
+		if(!$duplicaterequest)
+		{
+			print( "<p class = 'head'><input type = 'submit' name = 'submit' value = 'Register'></p></form>");
+		}
+		else
+		{
+			print "<p>This UMID " . $umid . " is already registered at time " . $_SESSION["previoustimeslot"] . ". Would you like to change it to time slot " . $_SESSION["timeslot"] . "?</p>";
+        print "<br><br>";
+        print '<script type="text/javascript">';
+        print 'appear()';
+        print '</script>';
+		}
+		print( "<h1>The Available Class List</h1>
+				<table>
+					<tr>
+						<th>Date and Time</th>
+						<th>Seats Remaining</th>
+					</tr>");
+		$result = mysql_query("SELECT * FROM regsection");
+		//execute the SQL query and return records
+		for($counter = 0;$row = mysql_fetch_row($result);$counter++)
+		{
+			print( "<tr>");
+			foreach($row as $key => $value)
+				print("<td>$value</td>");
+			print( "</tr>");
+		}
+		print( "</table>");
 
-</body>
+		//fetch tha data from the database 
+		//while ($row = mysql_fetch_array($result)) {
+		//   echo "ID:".$row{'UMID'}." Name:".$row{'FName'};
+		//}
+		//close the connection
+		mysql_close($database);
+		?>
+	</body>
 </html>
