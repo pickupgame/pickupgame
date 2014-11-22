e2e8fa80d4f19ad4a7da025b0865378a8ebf80af
unknown <jrussr@umich.edu>
2014-11-22 18:54:41 -0500

    changed file format to contain only php code

--- GetPassword.php
+++ GetPassword.php
@@ -1,21 +1,13 @@
-<html>
-	<head>
-		<meta charset = "utf-8">
-		<link rel = "stylesheet" type= "text/css" href = "index.css">
-		<title>Forgot Password</title>
-	</head>
-	<body>
-		<?php
-		session_start();
-		include('sql_functions.php');
-		$UserID = isset($_SESSION["UserIDforPasswordGet"]) ? $_SESSION[ "UserIDforPasswordGet" ] : "";
-		$Password= getPassword(getUserName($UserID));
-		print("<h1>Forgot Password</h1>");
-		print("<h3>Your Password is:</h3>");
-		print("<h3>{$Password}</h3>");
-		print("<form action= 'login.php'>");
-		print("<input type = 'submit' value='Return To Login'></p>");
-		print("</form>");
-		?>
-	</body>
-</html>
+<!-- GetPassword.php-->
+<?php
+session_start();
+include('sql_functions.php');
+$UserID = isset($_SESSION["UserIDforPasswordGet"]) ? $_SESSION[ "UserIDforPasswordGet" ] : "";
+$Password= getPassword(getUserName($UserID));
+print("<h1>Forgot Password</h1>");
+print("<h3>Your Password is:</h3>");
+print("<h3>{$Password}</h3>");
+print("<form action= 'login.php'>");
+print("<input type = 'submit' value='Return To Login'></p>");
+print("</form>");
+?>
