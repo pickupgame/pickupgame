ae96adccacaebedd094ed3ac87272d0789da4a2e
Jake <jsovoda@umich.edu>
2014-11-28 13:24:05 -0500

    modified home.php to include some game data

--- home.php
+++ home.php
@@ -4,6 +4,29 @@ include('db/sql_functions.php');
 
 $user = getPlayerDetails($_SESSION['UserID']);
 
-echo "Welcome {$user['UserName']}!";
+echo "Welcome {$user['UserName']}!<br>";
+$hostGames = getHostGames($user['UserID']);
+$playerGames = getPlayerGames($user['UserID']);
+
+if($hostGames)
+{
+	displayGames($hostGames, 'Host');
+}
+else
+{
+	echo "You are not a host. You have no hosted games. <br>";	
+}
+
+
+
+
+if($playerGames)
+{
+	displayGames($playerGames, 'Player');
+}
+else
+{
+	echo "You have not joined any games.";	
+}
 
 ?>
