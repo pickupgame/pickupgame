1a0df2a7a06fc5ef0763b32f200f0a07e9f4c4b8
Iskander1 <ibayyouk@gmail.com>
2014-12-01 22:57:58 -0500

    Browse &setlocation

--- browse.php
+++ browse.php
@@ -1,19 +1,48 @@
 <?php
-if(!isset($_GET['Game_ID']))
-{
+include("/db/sql_functions.php");
+	if(isset($_POST['formSubmit'])) 
+	{
+		$varSport = $_POST['formSport'];
+		if ($varSport == 'vb')
+		{
+		$query = "SELECT * FROM game WHERE Sport=?";
+		$rows = query("Volleyball",$query);
+		}
+		else if ($varSport == 'fb')
+		{
+		$query = "SELECT * FROM game WHERE Sport=?";
+		$rows = query("Football",$query);
+		}
+		else if ($varSport == 'sc')
+		{
+		$query = "SELECT * FROM game WHERE Sport=?";
+		$rows = query("Soccer",$query);
+		}
+		else 
+		{
+		$query = "SELECT * FROM game WHERE Sport LIKE ?";
+		$rows = query("%",$query);
+		}
+		echo '<table border="1" cellpadding="1" cellspacing="1" style="width: 500px;"> <tr> <td>Game Name</td> <td>Sport</td> <td>Start Time</td> <td>Private?</td> <td>View Game</td> </tr>';
+		if(!empty($rows))
+			{
+			foreach ($rows as $row) {
+		       echo "<tr>";
+		       echo "<td>".$row["GameName"]."</td>";
+		       echo "<td>".$row["Sport"]."</td>";
+		       echo "<td>".$row["DateAndTime"]."</td>";
+		       echo "<td>".$row["Private"]."</td>";
+		       echo "<td>".$row["Game_ID"]."</td>";
+		       echo "</tr>";
+		   		}
+			}
+	   echo "</table>";
+
+	}
 ?>
-<div>
-	<table class="table">
-		<th>Game</th>
-		<th>Host</th>
-		<th>Rating</th>
-		<th>Players</th>
-		<tr>
-			<td><a href="index.php?page=browse&Game_ID=6">Game 6</a></td>
-			<td>Random Host Name</td>
-			<td><span class="text-success">5</span></td>
-			<td>5/7</td>
-		</tr>
-	</table>
-</div>
-<?php } ?>
+
+<form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+	<label for='formSport'>Select a sport</label><br>
+	<select name="formSport"><option value="Select">All Games</option><option value="vb">Volleyball</option><option value="fb">Football</option><option value="sc">Soccer</option> </select>
+	<input type="submit" name="formSubmit" value="Submit" >
+</form>
