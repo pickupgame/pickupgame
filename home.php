7754f96d6f4a9d3625d12ea441e222322936bf92
Joshua D. Evich <joshcpa90@gmail.com>
2014-12-01 22:46:49 -0500

    Home.php includes HTML code only because of I was unsure of how to include CSS file in php code alone

---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+	<link rel="stylesheet" type="text/css" href="tabstyle.css">
+</head>
+<body>
+<?php
+	include('sql_functions.php');
+
+	generateSportsTabs();
+
+?>
+</body>
+</html>
